b0cf67306d6eb1ba3179e88b29922461b14f1a0e
she-sree <rajasreen23@gmail.com>
2024-03-11 04:25:16 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>WELCOME TO MEALS HOME</h3>
+      <img className="home-img" src="https://yourcoimbatore.com/wp-content/uploads/2021/12/Kongu-Nadu-samayal-Non-vegetarian-recipes.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
